3a9f3c9c29005c06c9b2f3f0caac4d1769624f6e
Deep-Kacha <deepnkacha2256@gmail.com>
2026-01-14 19:52:57 +0530

    learn to create a MVC project and create a DB

---
+++ BD/MVC-Demo/DBContext/studentDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Demo.Models.DBContext
+{
+    public class STUDENTDB
+    {
+        public void AddStudent(Student S)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\ASP.NET\\BD\\MVC-Demo\\App_Data\\StudentDB.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("insert into Student values(@studentname,@studentcity)", con);
+
+            cmd.Parameters.AddWithValue("@studentid", S.studentname);
+            cmd.Parameters.AddWithValue("@studentcity", S.studentcity);
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+        }
+    }
+}
---
+++ BD/MVC-Demo/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Demo.Models
+{
+    public class Student
+    {
+        [Key]
+
+        [Required(ErrorMessage = "ID Lakho Pela")]
+        public int studentid { get; set; }
+
+        [Required(ErrorMessage = "Name Lakho Pela")]
+        public string studentname { get; set; }
+        public string studentcity { get; set; }
+    }
+}
