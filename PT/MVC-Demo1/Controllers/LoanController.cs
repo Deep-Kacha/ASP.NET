f4029efd3bd9fab81b47609376fbd06d39dc47be
Deep-Kacha <deepnkacha2256@gmail.com>
2026-01-30 11:34:27 +0530

    learn a structure of a MVC .NET core and explore navigation

---
+++ PT/MVC-Demo1/Controllers/LoanController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Demo1.Controllers
+{
+    public class LoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewAll() // Action Method for ViewAll.cshtml
+        {
+            return View(); // it means shwow ViewAll.cshtml
+        }
+
+        public IActionResult NewApplication() // Action Method for NewApplication.cshtml
+        {
+            return View(); // it means show NewApplication.cshtml
+        }
+
+        public IActionResult UpdateApplication() // Action Method for UpdateApplication.cshtml
+        {
+            return View(); // it means show UpdateApplication.cshtml
+        }
+
+        public IActionResult DiscardApplication() // Action Method for DiscardApplication.cshtml
+        {
+            return View(); // it means show DiscardApplication.cshtml
+        }
+    }
+}
---
+++ PT/MVC-Demo1/Models/LoanModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MVC_Demo1.Models
+{
+    public class LoanModel
+    {
+        public int Id { get; set; }
+    }
+}
